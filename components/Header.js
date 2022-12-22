7cc2fabfefe679e3ac569cd0d9bb9a269c6c2a90
Muhamad Arsyad <marsyad@student.ittelkom-sby.ac.id>
2022-12-22 12:14:37 +0700

    Add safeareaview for header

--- components/Header.js
+++ components/Header.js
@@ -7,23 +7,26 @@ import {
   } from "native-base";
   import React from "react";
   import { useNavigation } from "@react-navigation/native";
+  import { SafeAreaView } from 'react-native-safe-area-context';
   
   const Header = ({ title, withBackBtn }) => {
     const navigation = useNavigation();
   
     return (
-      <Box bg="#272b30" paddingX={15} paddingY={2}>
-        <HStack alignItems="center">
-            {withBackBtn && (
-              <Pressable onPress={() => navigation.goBack()}>
-                <ArrowBackIcon color="white" size="md" marginRight="3" />
-              </Pressable>
-            )}
-            <Text color="white" fontWeight="semibold" fontSize="2xl">
-              {title}
-            </Text>
-        </HStack>
-      </Box>
+      <SafeAreaView>
+        <Box bg="#272b30" paddingX={15} paddingY={2}>
+          <HStack alignItems="center">
+              {withBackBtn && (
+                <Pressable onPress={() => navigation.goBack()}>
+                  <ArrowBackIcon color="white" size="md" marginRight="3" />
+                </Pressable>
+              )}
+              <Text color="white" fontWeight="semibold" fontSize="2xl">
+                {title}
+              </Text>
+          </HStack>
+        </Box>
+      </SafeAreaView>
     );
   };
   
